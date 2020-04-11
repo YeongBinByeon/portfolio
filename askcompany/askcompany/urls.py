8f9c98b1831f1a4633b7b660803af47537701c15
yeongbin.byeon <yeongbin6483@gmail.com>
2020-04-12 01:59:22 +0900

    3 bootstrap4를 활용한 기본 레이아웃 구현

--- askcompany/askcompany/urls.py
+++ askcompany/askcompany/urls.py
@@ -2,9 +2,11 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', TemplateView.as_view(template_name='root.html'), name='root'),
 ]
 
 #media 파일에 대한 static surve 기능
