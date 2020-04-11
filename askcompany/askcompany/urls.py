a4a1fd5b795af6b27761e41f2164a7442f7f06cb
yeongbin.byeon <yeongbin6483@gmail.com>
2020-04-12 00:21:36 +0900

    1,2 프로젝트 생성 및 초기 프로젝트 환경 설정 까지

---
+++ askcompany/askcompany/urls.py
@@ -0,0 +1,17 @@
+from django.conf import settings
+from django.contrib import admin
+from django.urls import include, path
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+]
+
+#media 파일에 대한 static surve 기능
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += [
+        path('__debug__/', include(debug_toolbar.urls)),
+    ]
+
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
