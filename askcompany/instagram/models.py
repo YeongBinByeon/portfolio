004de5d21102894f2c931c6454210a4fe66067e4
yeongbin.byeon <yeongbin6483@gmail.com>
2020-06-13 18:10:52 +0900

    20 - 포스팅 좋아요-취소 구현

--- askcompany/instagram/models.py
+++ askcompany/instagram/models.py
@@ -3,13 +3,22 @@ from django.conf import settings
 from django.db import models
 from django.urls import reverse
 
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
 # Create your models here.
-class Post(models.Model):
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+class Post(BaseModel):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='my_post_set', on_delete=models.CASCADE)
     photo = models.ImageField(upload_to="instagram/post/%Y/%m/%d")
     caption = models.CharField(max_length=500)
     tag_set = models.ManyToManyField('Tag', blank=True)
     location = models.CharField(max_length=100)
+    like_user_set = models.ManyToManyField(settings.AUTH_USER_MODEL, blank=True, related_name='liked_post_set')
+
 
     def __str__(self):
         return self.caption
@@ -25,6 +34,12 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return reverse("instagram:post_detail", args=[self.pk])
+
+    def is_like_user(self, user):
+        return self.like_user_set.filter(pk=user.pk).exists()
+
+    class Meta:
+        ordering = ['-id']
     
 
 class Tag(models.Model):
