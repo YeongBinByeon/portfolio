a988a31db3c6c54111e3a9fb63fec74c3a22496e
yeongbin.byeon <yeongbin6483@gmail.com>
2020-07-05 18:41:42 +0900

    10-2) 포스팅 목록 API를 구현하고 리액트에서 받아서 표현하기

--- django-with-react-rev5/backend/backend/settings/common.py
+++ django-with-react-rev5/backend/backend/settings/common.py
@@ -11,9 +11,10 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from os.path import abspath, dirname, join
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = dirname(dirname(dirname(abspath(__file__))))
 
 
 # Quick-start development settings - unsuitable for production
@@ -31,15 +32,23 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # django apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #third apps
+    "rest_framework",
+    "corsheaders",
+    #local apps
+    'instagram',
+    'accounts',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -99,6 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = "accounts.User"
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
@@ -118,3 +128,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+REST_FRAMEWORK = {'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ]
+}
---
+++ django-with-react-rev5/frontend/src/PostList.js
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from "react";
+import Axios from "axios";
+import Post from "Post";
+
+const apiUrl = "http://localhost:8000/api/posts/";
+
+function PostList() {
+  const [postList, setPostList] = useState([]);
+  useEffect(() => {
+    Axios.get(apiUrl)
+      //Axios.get() 호출시 결과 promise 객체 반환함. status code 400 미만이면 then 수행
+      .then((response) => {
+        const { data } = response;
+        console.log("loaded response :", response);
+        setPostList(data);
+      })
+      //status code 400 이상이면 예외 수행
+      .catch((error) => {
+        // error.response;
+      });
+    console.log("mounted");
+  }, []);
+  return (
+    <div>
+      <h2>PostList</h2>
+      {postList.map((post) => {
+        return <Post post={post} key={post.id} />;
+      })}
+    </div>
+  );
+}
+
+export default PostList;
