165423b43d825259acf8c2ffc080f30c4f94b92a
yeongbin.byeon <yeongbin6483@gmail.com>
2020-07-09 08:03:28 +0900

    10-4) CSS Grid와 Ant Design을 활용한 레이아웃

--- django-with-react-rev5/frontend/src/components/AppLayout.js
+++ django-with-react-rev5/frontend/src/components/AppLayout.js
@@ -1,16 +1,40 @@
 import React from "react";
-import AppHeader from "./AppHeader";
-import AppFooter from "./AppFooter";
+import "./AppLayout.scss";
+import { Input, Menu } from "antd";
+import SuggestionList from "./SuggestionList";
+import StoryList from "./StoryList";
+import LogoImage from "assets/logo.png";
 
 function AppLayout(props) {
   const { children } = props;
   console.log(props);
   return (
-    <>
-      <AppHeader />
-      {children}
-      <AppFooter />
-    </>
+    <div className="app">
+      <div className="header">
+        <h1 className="page-title">
+          <img src={LogoImage} alt="logo" />
+        </h1>
+        <div className="search">
+          <Input.Search />
+        </div>
+
+        <div className="topnav">
+          <Menu mode="horizontal">
+            <Menu.Item>메뉴1</Menu.Item>
+            <Menu.Item>메뉴2</Menu.Item>
+            <Menu.Item>메뉴3</Menu.Item>
+          </Menu>
+        </div>
+      </div>
+      <div className="contents">{children}</div>
+      <div className="sidebar">
+        <StoryList style={{ marginBottom: "1rem" }} />
+        <SuggestionList />
+      </div>
+      <div className="footer">
+        &copy; 2020. https://github.com/YeongBinByeon.
+      </div>
+    </div>
   );
 }
 
