4f875f3a7e8cd7c8b09339cdb9bc9b2aea7856da
yeongbin.byeon <yeongbin6483@gmail.com>
2020-07-15 08:36:04 +0900

    10-17) 포스팅 목록 API 및 화면 정리

--- django-with-react-rev5/frontend/src/components/Post.js
+++ django-with-react-rev5/frontend/src/components/Post.js
@@ -4,16 +4,28 @@ import { HeartOutlined, HeartFilled, UserOutlined } from "@ant-design/icons";
 import "./Post.scss";
 
 function Post({ post }) {
-  const { caption, location, photo } = post;
+  const { author, caption, location, photo, tag_set, like_user_set } = post;
+  const { username, name, avatar_url } = author;
   return (
     <div>
       <Card
         hoverable
         cover={<img src={photo} alt={caption} />}
+        //FIXME: host 지정을 로직으로 처리
         actions={[<HeartOutlined />]}
       >
         <Card.Meta
-          avatar={<Avatar size="large" icon={<UserOutlined />} />}
+          avatar={
+            <Avatar
+              size="large"
+              icon={
+                <img
+                  src={`http://localhost:8000` + avatar_url}
+                  alt={username}
+                />
+              }
+            />
+          }
           title={location}
           description={caption}
         />
