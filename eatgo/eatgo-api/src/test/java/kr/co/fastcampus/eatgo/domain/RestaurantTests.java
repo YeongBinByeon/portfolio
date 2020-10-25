a67ce33dcea0ed32183f398efd41f60eb46be1f1
yeongbin.byeon <yeongbin6483@gmail.com>
2020-10-26 00:44:25 +0900

    스프링 부트 프로젝트 (레스토랑 예약 사이트 만들기)

---
+++ eatgo/eatgo-api/src/test/java/kr/co/fastcampus/eatgo/domain/RestaurantTests.java
@@ -0,0 +1,17 @@
+package kr.co.fastcampus.eatgo.domain;
+
+
+import org.hamcrest.core.Is;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class RestaurantTests {
+
+    @Test
+    public void creation(){
+//        Restaurant restaurant = new Restaurant("Bob zip");
+//        assertThat(restaurant.getName(), Is.is("Bob zip"));
+    }
+
+}
