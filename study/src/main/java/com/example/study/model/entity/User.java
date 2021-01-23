cb003c19379e3c570031d25ce6c64646b10c21c5
yeongbin.byeon <yeongbin6483@gmail.com>
2021-01-23 15:26:17 +0900

    9장 Entity 생성과 Repository

---
+++ study/src/main/java/com/example/study/model/entity/User.java
@@ -0,0 +1,25 @@
+package com.example.study.model.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@Entity // ==table
+//@Table(name = "user")  //Entity의 Class 이름과 DB의 Table 이름이 동일하다면 @Table Annotation 안써도 JPA가 알아서 mapping해준다.
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String account;
+    // @Column(name = "account") // 변수의 명과 데이터베이스 column의 명이 동일하다면 이 annotation도 안써도 된다.
+    private String email;
+    private String phoneNumber;
+    private LocalDateTime createdAt;
+    private String createdBy;
+    private LocalDateTime updatedAt;
+    private String updatedBy;
+}
