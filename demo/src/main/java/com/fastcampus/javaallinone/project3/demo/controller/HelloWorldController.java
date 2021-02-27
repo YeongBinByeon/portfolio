6c46b11054cab8c2dfe456e2a17a602f64474fc9
yeongbin.byeon <yeongbin6483@gmail.com>
2021-02-28 02:18:26 +0900

    04. 스프링부트 시작 - 02. HelloWorldController 생성

---
+++ demo/src/main/java/com/fastcampus/javaallinone/project3/demo/controller/HelloWorldController.java
@@ -0,0 +1,12 @@
+package com.fastcampus.javaallinone.project3.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+    @GetMapping(value="/api/helloWorld")
+    public String helloWorld(){
+        return "HelloWorld";
+    }
+}
