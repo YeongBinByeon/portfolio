6f8018a52da012e97472f1eabb440574e5d6ac0b
yeongbin.byeon <yeongbin6483@gmail.com>
2021-03-02 00:33:09 +0900

    10. Chapter04. 01. JPA Relation - 1

---
+++ demo/src/test/java/com/fastcampus/javaallinone/project3/demo/repository/BlockRepositoryTest.java
@@ -0,0 +1,35 @@
+package com.fastcampus.javaallinone.project3.demo.repository;
+
+import com.fastcampus.javaallinone.project3.demo.domain.Block;
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class BlockRepositoryTest {
+
+    @Autowired
+    private BlockRepository blockRepository;
+
+    @Test
+    void crud(){
+        Block block = new Block();
+
+        block.setName("martin");
+        block.setReason("귀찮아서");
+        block.setStartDate(LocalDate.now());
+        block.setEndDate(LocalDate.now());
+
+        blockRepository.save(block);
+
+        List<Block> blocks = blockRepository.findAll();
+        Assert.assertEquals(blocks.size(), 1);
+        Assert.assertEquals(blocks.get(0).getName(), "martin");
+    }
+}
