ede587f132fb42c865d4f5671cebfc99c288d6ba
yeongbin.byeon <yeongbin6483@gmail.com>
2021-03-08 02:57:21 +0900

    22장Chapter 06. Controller-Test - 01. Controller Test #1

--- demo/src/test/java/com/fastcampus/javaallinone/project3/demo/controller/PersonControllerTest.java
+++ demo/src/test/java/com/fastcampus/javaallinone/project3/demo/controller/PersonControllerTest.java
@@ -1,8 +1,14 @@
 package com.fastcampus.javaallinone.project3.demo.controller;
 
+import com.fastcampus.javaallinone.project3.demo.controller.dto.PersonDto;
+import com.fastcampus.javaallinone.project3.demo.domain.Person;
+import com.fastcampus.javaallinone.project3.demo.domain.dto.Birthday;
 import com.fastcampus.javaallinone.project3.demo.repository.PersonRepository;
 
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Assert;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
@@ -14,9 +20,13 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.util.NestedServletException;
+
+import java.time.LocalDate;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -27,6 +37,9 @@ class PersonControllerTest {
 
     @Autowired
     private PersonRepository personRepository;
+
+    @Autowired
+    private ObjectMapper objectMapper;
 
     private MockMvc mockMvc;
 
@@ -59,35 +72,61 @@ class PersonControllerTest {
 
     @Test
     void modifyPerson() throws Exception{
+        PersonDto dto = PersonDto.of("martin", "programming", "판교", LocalDate.now(), "programmer", "010-1111-2222");
+
         mockMvc.perform(
                 MockMvcRequestBuilders.put("/api/person/1")
                         .contentType(MediaType.APPLICATION_JSON_UTF8)
-                        .content("{\n" +
-                                "    \"name\": \"martin\",\n" +
-                                "    \"age\": 20,\n" +
-                                "    \"bloodType\": \"A\"\n" +
-                                "}"))
+                        .content(toJsonString(dto)))
                 .andDo(print())
                 .andExpect(status().isOk());
+        Person result = personRepository.findById(1L).get();
+        
+        Assert.assertEquals(result.getName(),"martin");
+        Assert.assertEquals(result.getHobby(),"programming");
+        Assert.assertEquals(result.getAddress(),"판교");
+        Assert.assertEquals(result.getBirthday(), Birthday.of(LocalDate.now()));
+        Assert.assertEquals(result.getJob(),"programmer");
+        Assert.assertEquals(result.getPhoneNumber(),"010-1111-2222");
+
+        
+    }
+
+    @Test
+    void modifyPersonIfNameIsDifferent() throws Exception{
+        PersonDto dto = PersonDto.of("james", "programming", "판교", LocalDate.now(), "programmer", "010-1111-2222");
+
+        assertThrows(NestedServletException.class, ()->
+            mockMvc.perform(
+                    MockMvcRequestBuilders.put("/api/person/1")
+                            .contentType(MediaType.APPLICATION_JSON_UTF8)
+                            .content(toJsonString(dto)))
+                    .andDo(print())
+                    .andExpect(status().isOk()));
     }
 
     @Test
     void modifyName() throws Exception{
         mockMvc.perform(
                 MockMvcRequestBuilders.patch("/api/person/1")
-                        .param("name", "martin2"))
+                        .param("name", "martinModified"))
                 .andDo(print())
                 .andExpect(status().isOk());
+        Assert.assertEquals(personRepository.findById(1L).get().getName(), "martinModified");
     }
 
     @Test
-    @Disabled
     void deletePerson() throws Exception{
         mockMvc.perform(MockMvcRequestBuilders.delete("/api/person/1"))
                 .andDo(print())
                 .andExpect(status().isOk());
         //log.info("people deleted : {}", personRepository.findPeopleDeleted());
+        Assert.assertEquals(personRepository.findPeopleDeleted().stream().anyMatch(person -> person.getId().equals(1L)), true);
         System.out.println(personRepository.findPeopleDeleted());
+    }
+
+    private String toJsonString(PersonDto personDto) throws JsonProcessingException {
+        return objectMapper.writeValueAsString(personDto);
     }
 
 
