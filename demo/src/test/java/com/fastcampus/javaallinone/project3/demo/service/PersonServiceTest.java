956f0e7031073e645ee4bc73cd33a0704e9f933a
yeongbin.byeon <yeongbin6483@gmail.com>
2021-03-08 07:31:16 +0900

    Chapter 08. Mock-Test - 01. Service Test #!

--- demo/src/test/java/com/fastcampus/javaallinone/project3/demo/service/PersonServiceTest.java
+++ demo/src/test/java/com/fastcampus/javaallinone/project3/demo/service/PersonServiceTest.java
@@ -1,34 +1,36 @@
 package com.fastcampus.javaallinone.project3.demo.service;
 
 import com.fastcampus.javaallinone.project3.demo.domain.Person;
+import com.fastcampus.javaallinone.project3.demo.repository.PersonRepository;
+import org.assertj.core.util.Lists;
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
 
-@SpringBootTest
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 class PersonServiceTest {
-    @Autowired
+    @InjectMocks
     private PersonService personService;
-
-    @Test
-    void getPerson(){
-
-        Person person = personService.getPerson(3L);
-
-        Assert.assertEquals(person.getName(), "dennis");
-
-    }
+    @Mock
+    private PersonRepository personRepository;
 
     @Test
     void getPeopleByName(){
-
+        when(personRepository.findByName("martin"))
+                .thenReturn(Lists.newArrayList(new Person("martin")));
         List<Person> result = personService.getPeopleByName("martin");
 
-        Assert.assertEquals(result.size(), 1);
-        Assert.assertEquals(result.get(0).getName(), "martin");
+        Assert.assertEquals(result.size(),1);
+        Assert.assertEquals(result.get(0).getName(),"martin");
     }
 
 }
