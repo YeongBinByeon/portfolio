c8df1b99c7545ff886cf0e631bbd4184362aaf19
yeongbin.byeon <yeongbin6483@gmail.com>
2021-03-09 06:00:25 +0900

    27장 Chapter 08. Mock-Test - 02 Service Test #2

--- demo/src/test/java/com/fastcampus/javaallinone/project3/demo/service/PersonServiceTest.java
+++ demo/src/test/java/com/fastcampus/javaallinone/project3/demo/service/PersonServiceTest.java
@@ -1,5 +1,6 @@
 package com.fastcampus.javaallinone.project3.demo.service;
 
+import com.fastcampus.javaallinone.project3.demo.controller.dto.PersonDto;
 import com.fastcampus.javaallinone.project3.demo.domain.Person;
 import com.fastcampus.javaallinone.project3.demo.repository.PersonRepository;
 import org.assertj.core.util.Lists;
@@ -12,9 +13,12 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
-import static org.mockito.Mockito.when;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class PersonServiceTest {
@@ -31,6 +35,30 @@ class PersonServiceTest {
 
         Assert.assertEquals(result.size(),1);
         Assert.assertEquals(result.get(0).getName(),"martin");
+    }
+
+    @Test
+    void getPerson(){
+        when(personRepository.findById(1L))
+                .thenReturn(Optional.of(new Person("martin")));
+        Person person = personService.getPerson(1L);
+        Assert.assertEquals(person.getName(), "martin");
+    }
+
+    @Test
+    void getPersonIfNotFound(){
+        when(personRepository.findById(1L))
+                .thenReturn(Optional.empty());
+        Person person = personService.getPerson(1L);
+        Assert.assertNull(person);
+    }
+
+    @Test
+    void put(){
+        PersonDto dto = PersonDto.of("martin", "programming", "판교", LocalDate.now(), "programmer", "010-1111-2222");
+        personService.put(dto);
+
+        verify(personRepository, times(1)).save(any(Person.class));
     }
 
 }
