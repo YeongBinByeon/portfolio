0e39e573fad7de88388ea515287efa76b84b5eb2
yeongbin.byeon <yeongbin6483@gmail.com>
2022-05-21 21:49:59 +0900

    Configuration Service 전체 커밋, Native File Repository 파트는 제외

---
+++ spring-msa-study/config-servcie/src/test/java/com/example/configservcie/ConfigServcieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.configservcie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServcieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- spring-msa-study/user-service/src/main/java/com/example/userservice/security/WebSecurity.java
+++ spring-msa-study/user-service/src/main/java/com/example/userservice/security/WebSecurity.java
@@ -30,11 +30,12 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
-        //http.authorizeRequests().antMatchers("/users/**").permitAll();
-        http.authorizeRequests().antMatchers("/users/**")
-                .hasIpAddress("127.0.0.1") // 본인 IP
-                .and()
-                .addFilter(getAuthenticationFilter());
+        http.authorizeRequests().antMatchers("/**").permitAll();
+        http.authorizeRequests().antMatchers("/actuator/**").permitAll();
+//        http.authorizeRequests().antMatchers("/**")
+//                .hasIpAddress("127.0.0.1") // 본인 IP
+//                .and()
+//                .addFilter(getAuthenticationFilter());
 
         http.headers().frameOptions().disable();
     }
