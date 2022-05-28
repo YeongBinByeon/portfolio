65455568bd9fc9bed0e1f9a6a80e45dee63847f2
yeongbin.byeon <yeongbin6483@gmail.com>
2022-05-28 16:30:38 +0900

    Microservice간 통신 전체 내용 커밋

---
+++ spring-msa-study/user-service/src/main/java/com/example/userservice/client/OrderServiceClient.java
@@ -0,0 +1,18 @@
+package com.example.userservice.client;
+
+
+import com.example.userservice.vo.ResponseOrder;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
+// micro service 이름을 FeignClient에 넣어야 함
+@FeignClient(name = "order-service")
+public interface OrderServiceClient {
+
+    @GetMapping("/order-service/{userId}/orders")
+    List<ResponseOrder> getOrders(@PathVariable String userId);
+
+}
--- spring-msa-study/user-service/src/main/java/com/example/userservice/service/UserServiceImpl.java
+++ spring-msa-study/user-service/src/main/java/com/example/userservice/service/UserServiceImpl.java
@@ -1,9 +1,12 @@
 package com.example.userservice.service;
 
+import com.example.userservice.client.OrderServiceClient;
 import com.example.userservice.dto.UserDto;
 import com.example.userservice.jpa.UserEntity;
 import com.example.userservice.jpa.UserRepository;
 import com.example.userservice.vo.ResponseOrder;
+import feign.FeignException;
+import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.modelmapper.spi.MatchingStrategy;
@@ -24,6 +27,7 @@ import java.util.List;
 import java.util.UUID;
 
 @Service
+@Slf4j
 public class UserServiceImpl implements UserService{
 
     UserRepository userRepository;
@@ -31,6 +35,8 @@ public class UserServiceImpl implements UserService{
 
     Environment env;
     RestTemplate restTemplate;
+
+    OrderServiceClient orderServiceClient;
 
 
     @Override
@@ -50,11 +56,13 @@ public class UserServiceImpl implements UserService{
     public UserServiceImpl(UserRepository userRepository,
                            BCryptPasswordEncoder passwordEncoder,
                            Environment env,
-                            RestTemplate restTemplate){
+                            RestTemplate restTemplate,
+                           OrderServiceClient orderServiceClient){
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
         this.env = env;
         this.restTemplate = restTemplate;
+        this.orderServiceClient = orderServiceClient;
     }
 
     @Override
@@ -87,13 +95,25 @@ public class UserServiceImpl implements UserService{
         //List<ResponseOrder> orders = new ArrayList<>();
 
         /* Using as resttemplate */
-        String orderUrl = String.format(env.getProperty("order_service.url"), userId);
-        ResponseEntity<List<ResponseOrder>> orderListResponse =
-                restTemplate.exchange(orderUrl, HttpMethod.GET, null,
-                    new ParameterizedTypeReference<List<ResponseOrder>>() {
-        });
+//        String orderUrl = String.format(env.getProperty("order_service.url"), userId);
+//        ResponseEntity<List<ResponseOrder>> orderListResponse =
+//                restTemplate.exchange(orderUrl, HttpMethod.GET, null,
+//                    new ParameterizedTypeReference<List<ResponseOrder>>() {
+//        });
+//        List<ResponseOrder> ordersList = orderListResponse.getBody();
 
-        List<ResponseOrder> ordersList = orderListResponse.getBody();
+        /* Using a feign client*/
+        /* Feign Exception Handling */
+//        List<ResponseOrder> ordersList = null;
+//
+//        try{
+//            ordersList = orderServiceClient.getOrders(userId);
+//        }catch(FeignException ex){
+//            log.error(ex.getMessage());
+//        }
+
+        /* ErrorDecoder */
+        List<ResponseOrder> ordersList = orderServiceClient.getOrders(userId);
 
         userDto.setOrders(ordersList);
 
