83fcdaeeb132efa369fe6e5e9fc878a872ca2bb7
yeongbin.byeon <yeongbin6483@gmail.com>
2022-07-13 01:35:17 +0900

    Configuration Service 전체 내용 커밋

--- spring-msa-study-review/user-service/src/main/java/com/example/userservice/security/WebSecurity.java
+++ spring-msa-study-review/user-service/src/main/java/com/example/userservice/security/WebSecurity.java
@@ -32,7 +32,8 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
 //        http.authorizeRequests().antMatchers("/users/**").permitAll();
-        http.authorizeRequests().antMatchers("/**")
+       http.authorizeRequests().antMatchers("/actuator/**").permitAll();
+       http.authorizeRequests().antMatchers("/**")
                 .hasIpAddress("host.docker.internal") // 본인 IP
                 //.hasIpAddress("127.0.0.1") // 본인 IP
                 .and()
