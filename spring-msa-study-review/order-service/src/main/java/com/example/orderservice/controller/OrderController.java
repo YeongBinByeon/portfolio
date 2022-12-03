c744610074f58967f6b3b5c116f83f792e3f9b48
yeongbin <yeongbin6483@gmail.com>
2022-12-04 08:09:52 +0900

    Catalogs, OrderService에 Kafka Topic 적용한 내용 커밋

--- spring-msa-study-review/order-service/src/main/java/com/example/orderservice/controller/OrderController.java
+++ spring-msa-study-review/order-service/src/main/java/com/example/orderservice/controller/OrderController.java
@@ -3,6 +3,7 @@ package com.example.orderservice.controller;
 import com.example.orderservice.dto.OrderDto;
 import com.example.orderservice.jpa.OrderEntity;
 
+import com.example.orderservice.messagequeue.KafkaProducer;
 import com.example.orderservice.service.OrderService;
 import com.example.orderservice.vo.RequestOrder;
 import com.example.orderservice.vo.ResponseOrder;
@@ -24,12 +25,14 @@ import java.util.List;
 public class OrderController {
     Environment env;
     OrderService orderService;
+    KafkaProducer kafkaProducer;
 
 
     @Autowired
-    public OrderController(Environment env, OrderService orderService) {
+    public OrderController(Environment env, OrderService orderService, KafkaProducer kafkaProducer) {
         this.env = env;
         this.orderService = orderService;
+        this.kafkaProducer = kafkaProducer;
     }
 
     @GetMapping("/health_check")
@@ -59,7 +62,7 @@ public class OrderController {
 
 
         /* send this order to the kafka */
-//        kafkaProducer.send("example-catalog-topic", orderDto);
+        kafkaProducer.send("example-catalog-topic", orderDto);
 //        orderProducer.send("orders", orderDto);
 //
 //        ResponseOrder responseOrder = mapper.map(orderDto, ResponseOrder.class);
